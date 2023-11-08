48ea40671eee1725e806c2d57661264e28d9cfbb
thangplc <thangplc1412@gmail.com>
2023-11-08 10:29:32 +0700

    Add docs

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api.us-east-1.elasticbeanstalk.com/api/v0',
-  // apiHost: 'http://localhost:8080/api/v0',
+  // apiHost: 'http://udagram-api.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://localhost:8080/api/v0',
 };
 
 /*
